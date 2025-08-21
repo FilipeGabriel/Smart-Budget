e83d12953e97089e026da1b5c72df2a800724ca4
Filipe Gabriel <filipegabriel11@hotmail.com>
2025-08-20 23:39:17 -0300

    feat - first commit

---
+++ src/test/java/com/filipegabriel/smart_budget/SmartBudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filipegabriel.smart_budget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartBudgetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
